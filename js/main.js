6b2fcefed0d8125eb84c06e999a37f678950876a
Evgeniyatorva <Evgeniyatorva@mail.ru>
2017-10-12 08:22:31 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -20,9 +20,23 @@ $('#slider2').slick({
 });
 
 $('#slider3').slick({
-	slidesToShow: 3,
 	prevArrow: '<button type="button" class="slick-prev"><img src="img/slider__arrow/arrow_left.png"></button>',
-	nextArrow: '<button type="button" class="slick-next"><img src="img/slider__arrow/arrow_right.png"></button>'
+	nextArrow: '<button type="button" class="slick-next"><img src="img/slider__arrow/arrow_right.png"></button>',
+	slidesToShow: 3,
+	responsive: [
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+	 ]
 });
 
 $("#slider2 .slick-next").addClass("next2");
@@ -35,3 +49,13 @@ $("#slider3 .slick-prev").addClass("prev3");
 //tabs
 
 $( "#tabs" ).tabs();
+
+
+//mobile menu
+
+$(document).ready(function(){
+	$('.mobile-menu').click(function(){
+		$('.menu').slideToggle(1500);
+		return false
+	});
+});
